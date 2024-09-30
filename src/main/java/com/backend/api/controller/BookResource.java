b1d29e804bf714829f83a6e58cf239820fd577a7
claireglowup <claireglowup@gmail.com>
2024-10-01 01:12:02 +0700

    add post controller

--- src/main/java/com/backend/api/controller/BookResource.java
+++ src/main/java/com/backend/api/controller/BookResource.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -42,10 +44,35 @@ public class BookResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createBook (Book book) {
+    public Response createBook(Book book) {
         Book createdBook = bookService.createBook(book);
         return Response.status(Response.Status.CREATED).entity(createdBook).build();
     }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateBook(@PathParam("id") Long id,Book book) {
+        Book updatedBook = bookService.updateBook(id, book);
+        if (updatedBook != null) {
+           return Response.ok(updatedBook).build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteBook(@PathParam("id") Long id) {
+        boolean deletedBook = bookService.deleteBook(id);
+        if (deletedBook) {
+            return Response.noContent().build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
     
 
 }
